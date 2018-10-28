d0cbf11a48ec2ef59ac963194739b4a3e7ef395c
tomcw <tomcw@users.noreply.github.com>
2018-10-28 09:21:17 +0000

    Added tests for slot-0 LC and Saturn (GH408)

--- v2/Memory/test.bat
+++ v2/Memory/test.bat
@@ -1,6 +1,11 @@
 @IF "%~1" == "" GOTO help
 
 %1\applewin -log -load-state %~dp0\LC-RamWrite-Test.aws.yaml
+%1\applewin -log -load-state %~dp0\LC-RamWrite-Test-II+.aws.yaml
+%1\applewin -log -load-state %~dp0\LC-RamWrite-Test-II+Saturn.aws.yaml
+%1\applewin -log -load-state %~dp0\Saturn64.aws.yaml
+%1\applewin -log -load-state %~dp0\Saturn128.aws.yaml
+%1\applewin -log -load-state %~dp0\Saturn128-Bank3-2.aws.yaml
 %1\applewin -log -load-state %~dp0\RamWorks_r4.aws.yaml
 @ECHO Done v2 Memory tests
 @GOTO end
