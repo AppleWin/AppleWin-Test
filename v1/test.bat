d07ca5904b87c4f1d3da59cb52a15ff544ee6094
tomcw <tomcw@users.noreply.github.com>
2019-04-06 18:03:52 +0100

    Reduced tests for old v1 .aws format

--- v1/test.bat
+++ v1/test.bat
@@ -1,7 +1,6 @@
 @IF "%~1" == "" GOTO help
 
 %1\applewin -log -load-state %~dp0\Boulderdash1-level-L1-bonus.aws
-%1\applewin -log -load-state "%~dp0\Maniac Mansion(entrance-hall-near-clock).aws"
 @ECHO Done v1 tests
 @GOTO end
 
