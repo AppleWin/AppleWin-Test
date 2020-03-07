9a161f3a81cb5116503ee8e588496bd77cbf0016
tomcw <tomcw@users.noreply.github.com>
2020-03-07 18:22:05 +0000

    Added the 6522 tests to test.bat

--- v2/test.bat
+++ v2/test.bat
@@ -8,6 +8,7 @@
 %1\applewin -log -load-state %~dp0\audit-A2e-enhanced.aws.yaml
 CALL "v2\Video ROM\test.bat" %~1
 %1\applewin -log -load-state %~dp0\irq-pending.aws.yaml
+%1\applewin -log -load-state %~dp0\6522-one-shot.aws.yaml
 %1\applewin -log -load-state %~dp0\SmartPort-page-cross.aws.yaml
 %1\applewin -log -load-state %~dp0\Buttons_KeyData_AnyKeyDown-Test.aws.yaml -alt-enter=open-apple-enter
 %1\applewin -log -load-state %~dp0\archon_i.aws.yaml -video-mode=rgb-monitor -video-style=no-vertical-blend
