790b07d07d32cfedf81dcd01e80b2e526159cae2
tomcw <tomcw@users.noreply.github.com>
2020-03-26 22:38:55 +0000

    Added 2 tests for custom ROMs (GH771)

--- v2/Video ROM/test.bat
+++ v2/Video ROM/test.bat
@@ -8,6 +8,8 @@
 %1\applewin -log -videorom "%~dp0\Apple IIe Video French Canadian - Enhanced - 2732.bin" -load-state "%~dp0\A2e-enhanced.aws.yaml"
 %1\applewin -log -videorom "%~dp0\Apple IIe Video - Custom - French-US - Enhanced - 2764.bin" -load-state "%~dp0\A2e-enhanced.aws.yaml"
 %1\applewin -log -videorom "%~dp0\VID-342-274A.bin" -load-state "%~dp0\A2e-enhanced.aws.yaml"
+%1\applewin -log -rom "%~dp0\IIJPLUS.ROM" -videorom "%~dp0\JVIDEO.ROM" -load-state "%~dp0\A2+.aws.yaml"
+%1\applewin -log -rom "%~dp0\Apple2e_EDM.rom" -load-state "%~dp0\A2e-enhanced-EDM.aws.yaml"
 @ECHO Done v2 video ROM tests
 @GOTO end
 
