93a68fb017841b5e7b0fe99fff732cfc399af013
tomcw <tomcw@users.noreply.github.com>
2020-05-28 21:20:00 +0100

    Improved tests for -s7-empty-on-exit, etc (GH790)

--- v2/test.bat
+++ v2/test.bat
@@ -56,17 +56,19 @@ CALL "v2\Video ROM\test.bat" %~1
 %1\applewin -log -load-state "%~dp0\SSC-TCP-IN#2.aws.yaml"
 %1\applewin -log -load-state "%~dp0\SSC-TCP-tx-rx-tcp.aws.yaml"
 %1\applewin -log -load-state "%~dp0\SSC-COM2-tx-rx-tcp.aws.yaml"
+%1\applewin -log -h1 %~dp0\Dummy-image.hdv.gz
 %1\applewin -log -d1 %~dp0\AS-S2.dsk -s7 empty
-@REM
 %1\applewin -log -d1 %~dp0\AS-S1.dsk -d2 "%~dp0\AS-S2.dsk"
+@REM
+%1\applewin -log -d1 %~dp0\Dummy-image.dsk.gz
 %1\applewin -log -power-on
-%1\applewin -log -h1 %~dp0\GNR-FINAL.2mg.gz -s7-empty-on-exit
+%1\applewin -log -h1 %~dp0\Dummy-image.hdv.gz -s7-empty-on-exit
 %1\applewin -log -power-on
 @REM
 copy /y %~dp0\conftest-gm.ini %~dp0\conftest.ini
-%1\applewin -log -conf %~dp0\conftest.ini -d1 %~dp0\AS-S1.dsk -d2 "%~dp0\AS-S2.dsk"
+%1\applewin -log -conf %~dp0\conftest.ini -d1 %~dp0\Dummy-image.dsk.gz -d2 "%~dp0\AS-S2.dsk"
 %1\applewin -log -conf %~dp0\conftest.ini -power-on
-%1\applewin -log -conf %~dp0\conftest.ini -h1 %~dp0\GNR-FINAL.2mg.gz -s7-empty-on-exit
+%1\applewin -log -conf %~dp0\conftest.ini -h1 %~dp0\Dummy-image.hdv.gz -s7-empty-on-exit
 %1\applewin -log -conf %~dp0\conftest.ini -power-on
 @REM
 %1\applewin -log -h1 "%~dp0\Prince of Persia (Original 3.5 floppy for IIc+).2mg" -h2 %~dp0\GNR-FINAL.2mg.gz
