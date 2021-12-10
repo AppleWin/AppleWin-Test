1e4ff288a17d6922f9e509da429ba46f4c82cda4
tomcw <tomcw@users.noreply.github.com>
2021-12-10 19:48:21 +0000

    Add HDD r/w I/O boundary tests (GH1007)

--- v2/test.bat
+++ v2/test.bat
@@ -13,6 +13,7 @@ CALL "v2\Video ROM\test.bat" %1
 %1\applewin -log -load-state %~dp0\6522-one-shot.aws.yaml
 %1\applewin -log -load-state %~dp0\SSI263-test.aws.yaml
 %1\applewin -log -load-state %~dp0\SmartPort-page-cross.aws.yaml
+%1\applewin -log -load-state %~dp0\HDD-RW-IO-Boundary.aws.yaml
 %1\applewin -log -load-state %~dp0\Buttons_KeyData_AnyKeyDown-Test.aws.yaml -alt-enter=open-apple-enter
 %1\applewin -log -load-state %~dp0\4Play-slot4.aws.yaml
 %1\applewin -log -load-state %~dp0\SNES_MAX-slot4.aws.yaml
