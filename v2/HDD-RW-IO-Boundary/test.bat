3c265927bd42c9805f76e4cf43dd9d50e52a42ba
tomcw <tomcw@users.noreply.github.com>
2022-06-06 20:02:46 +0100

    HDD-RW-IO-Boundary: extend for debugger's HDD DMA breakpoints

---
+++ v2/HDD-RW-IO-Boundary/test.bat
@@ -0,0 +1,10 @@
+@IF "%~1" == "" GOTO help
+
+%1\applewin -log -load-state "%~dp0\HDD-RW-IO-Boundary.aws.yaml" -current-dir "%~dp0" -s7-empty-on-exit
+@ECHO Done v2 HDD-RW-IO-Boundary tests
+@GOTO end
+
+:help
+@ECHO %0 "<path to AppleWin under test>"
+
+:end
